16e287229fa790d8c57c993de9771962047ae92e
Sota529 <za446131@gmail.com>
2021-01-22 04:35:11 +0900

    Button完成

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,32 @@ import Header from "./components/Header";
 import { makeStyles } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
-import Button from "@material-ui/core/Button";
+import MyButon from "./components/MyButon";
+
 import Icon1 from "./images/Frame.svg";
 const useStyles = makeStyles((theme) => ({
   main: {
     paddingTop: 80,
-    textAlign: "center",
   },
   title: {
     fontSize: 24,
+    textAlign: "center",
   },
   sub_title: {
     fontSize: 14,
+    textAlign: "center",
   },
-  buttons:{
-    '& > *': {
+  buttons: {
+    "& > *": {
       margin: theme.spacing(1),
-      background:'#2979FF',
-      color:'white',
     },
-  }
+    textAlign: "center",
+  },
+  Icon: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
 }));
 function App() {
   const classes = useStyles();
@@ -36,22 +42,9 @@ function App() {
           <h2 className={classes.title}>物件を探す</h2>
         </div>
         <Container maxWidth="md" className={classes.area}>
-          <h3 className={classes.sub_title}>地域別に探す</h3>
-          <div className={classes.buttons}>
-            <Button variant="contained" >
-              千代田区
-            </Button>
-            <Button variant="contained" >
-              中央区
-            </Button>
-            <Button variant="contained" >
-              新宿区
-            </Button>
-            <Button variant="contained" >
-              港区
-            </Button>
-          </div>
-          <img src={Icon1} alt=""/>
+          <h3 className={classes.sub_title}>大学名</h3>
+          <MyButon />
+          <img className={classes.Icon} src={Icon1} alt="" />
         </Container>
       </div>
     </div>
