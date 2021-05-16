b7ef51fab5d0a8de204cd625c7c010135236d44b
Sota529 <za446131@gmail.com>
2021-05-16 21:21:55 +0900

    ✨ load再度追加

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import Layout from "../components/templates/layout.jsx";
+import { Center, ChakraProvider, Spinner } from "@chakra-ui/react";
+import Layout from "../components/templates/layout";
 import React, { useState, createContext, useEffect } from "react";
+import { useRouter } from "next/router";
 import { auth } from "../lib/db";
 
 export const AuthContext = createContext({ currentUser: undefined });
 
 function MyApp({ Component, pageProps }) {
+  const [load, setload] = useState(false);
   const [currentUser, setCurrentUser] = useState(undefined);
+  const router = useRouter();
+  router.events?.on("routeChangeStart", () => setload(true));
+  router.events?.on("routeChangeComplete", () => setload(false));
+  router.events?.on("routeChangeError", () => setload(false));
 
   useEffect(() => {
     auth.onAuthStateChanged(async (user) => {
@@ -18,9 +24,15 @@ function MyApp({ Component, pageProps }) {
     <>
       <ChakraProvider>
         <AuthContext.Provider value={currentUser}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          {load ? (
+            <Center pos="absolute" top="50vh" left="50vw">
+              <Spinner size="xl" thickness="3px" />
+            </Center>
+          ) : (
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          )}
         </AuthContext.Provider>
       </ChakraProvider>
     </>
