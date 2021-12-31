2a9ad7d0af565d5c392fbe78d8e08342153fa21f
Sota Hirose <za446131@gmail.com>
2021-12-31 20:03:53 +0900

    家を表示するのに必要な情報の型を作る

---
+++ pages/type.ts
@@ -0,0 +1,12 @@
+export interface HouseInfoType {
+  doc: string;
+  favo: boolean;
+  favoUser: string[];
+  id: string;
+  images: string[];
+  location: string;
+  name: string;
+  price: number;
+  thumbnail: string;
+  univ: string;
+}
