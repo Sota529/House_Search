18e9961ac00fecabb7ffaa819d2f017bf4d6c5f7
Sota Hirose <za446131@gmail.com>
2022-01-03 20:17:46 +0900

    CDのためのコミット

---
+++ __tests__/api/favo.test.ts
@@ -0,0 +1,7 @@
+// import doFavo from "../pages/api/favo";
+
+// test("正常系 ", () => {
+//     expect(doFavo()).toBeCalled()
+// });
+
+export {};
---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,11 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from "react"
+import Home from '../../src/pages/index'
+import { render } from '../utils/index'
+
+test('ホームページ', () => {
+  const { container } = render(<Home />)
+  expect(container).toMatchSnapshot()
+})
